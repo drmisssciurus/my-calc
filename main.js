ec152810c5e1c38363cfdd007f029ed842535c41
Alina <ms.sciurus@gmail.com>
2024-08-29 15:17:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// summ function
+
+const sum = (a, b) => a + b;
+
+// substraction function
+
+const sub = (a, b) => a - b
+
+// division function
+
+const divis = (a, b) => a / b
+
+// multiplication function
+
+const mult = (a, b) => a * b
+
+console.log(sum(2, 3));
+
